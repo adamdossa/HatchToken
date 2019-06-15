9217bf38f9db505f4ab12ae5dfe1ff19211a7c86
Adam Dossa <adam.dossa@gmail.com>
2019-06-15 19:43:39 -0400

    Multiple amounts by 10**18

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const LockedToken = artifacts.require("./LockedToken.sol");
 
 const BigNumber = require('bignumber.js');
+let wei = 1000000000000000000;
 
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(LockedToken, "Hatchtoken", "HATCH", BigNumber(18), BigNumber(1000000000), accounts[1], BigNumber(200000000), BigNumber(13300000));
+  web3.utils.toBN(BigNumber(String(1000000000)).times(String(wei)));
+  await deployer.deploy(LockedToken, "Hatchtoken", "HATCH", BigNumber(18), web3.utils.toBN(BigNumber(String(1000000000)).times(String(wei))), accounts[1], web3.utils.toBN(BigNumber(String(200000000)).times(String(wei))), web3.utils.toBN(BigNumber(String(13300000)).times(String(wei))));
   let lockedToken = await LockedToken.deployed();
   console.log("LockedToken: " + lockedToken.address);
   console.log("Treasury: " + await lockedToken.treasury());
