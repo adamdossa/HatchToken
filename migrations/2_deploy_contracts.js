084f9aef5229025135342ebc52b51ac3f5aceff2
Adam Dossa <adam.dossa@gmail.com>
2019-06-15 19:50:07 -0400

    And fix again

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const LockedToken = artifacts.require("./LockedToken.sol");
 const BigNumber = require('bignumber.js');
 
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(LockedToken, "Hatchtoken", "HATCH", BigNumber(18), web3.utils.toBN(String(1000000000)), accounts[1], web3.utils.toWei(String(200000000)), web3.utils.toBN(String(13300000)));
+  await deployer.deploy(LockedToken, "Hatchtoken", "HATCH", BigNumber(18), web3.utils.toWei(String(1000000000)), accounts[1], web3.utils.toWei(String(200000000)), web3.utils.toWei(String(13300000)));
   let lockedToken = await LockedToken.deployed();
   console.log("LockedToken: " + lockedToken.address);
   console.log("Treasury: " + await lockedToken.treasury());
